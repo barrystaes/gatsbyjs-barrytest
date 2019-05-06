078224a2f82e571aca81878ceae329d9712be581
Barry Staes <barry@staes.nl>
2019-05-06 20:29:15 +0200

    Add pages and menu

---
+++ src/pages/services.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const services = () => {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>Out aan links rechts bla</p>
+      <p>Meer tekst paragraaf bla</p>
+    </div>
+  )
+}
+
+export default services
